376fa074de1a2abd6dac4de978ea4a561124c630
rami saba <ramisaba02@gmail.com>
2022-02-20 13:07:06 +0100

    Add project files.

---
+++ Ramis Cinema/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ramis_Cinema.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
